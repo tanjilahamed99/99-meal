391838da65c07d11c1f658cd1df06aedd893f444
Tanjil Ahamed <tanjil113355@gmail.com>
2023-09-21 13:02:29 +0600

    header and footer component added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
-        <div>
-            <h2><span>99</span>Meal Box</h2>
-            <nav>
-                
+        <div className="flex justify-between container mx-auto pt-5">
+            <h2 className="text-2xl font-bold"><span className="text-purple-500">99 </span>Meal Box</h2>
+            <nav className="flex gap-10 text-xl font-semibold">
+                <Link>Home</Link>
+                <Link>Meal</Link>
+                <Link>About</Link>
             </nav>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import MainUi from './MainUi.jsx'
+import Home from './pages/Home'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainUi></MainUi>,
     children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+       
+      },
+      {
 
+      }
     ]
   },
   {
